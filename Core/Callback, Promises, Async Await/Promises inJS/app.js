2e573a08893401f911ef579be937cd48249a7129
thegospels <journeytomaang@gmail.com>
2025-01-24 00:31:29 +0530

    Initial commit

---
+++ Core/Callback, Promises, Async Await/Promises inJS/app.js
@@ -0,0 +1,22 @@
+
+
+console.log('This is a promise');
+
+
+let prom1 = new Promise((resolve,reject) => {
+  let a = Math.random();
+  if(a>0.5){
+    reject("Man this is not done, the script should uninstall the project")
+  }
+  else{
+    console.log('Fetching the data...')
+    setTimeout(() => {
+      console.log('Thank you for continuing')
+      resolve("We are working")
+    }, 2000);
+  }
+}).then((text,num)=> {
+  console.log(text);
+}).catch((err)=>{
+  console.log(`Man we got an error ${err}`)
+})
